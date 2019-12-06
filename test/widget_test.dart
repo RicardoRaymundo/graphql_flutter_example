a64e657a55a7cfb927445a175ee16a33a1b678e3
macbookpro <1>
2019-12-06 12:18:42 -0300

    Desenvolvendo logica para web

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:graphql_flutter_example/main.dart';
+import 'package:graphql_flutter_example/main_mobile.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
